7e2be6dcf0a884eb7c5a74500600ca98ca3a03e3
Wolvz <peng86david@gmail.com>
2019-02-19 22:35:10 +0800

    object shattering POC

---
+++ Assets/POC/destructable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destructable : MonoBehaviour {
+	
+	public GameObject destroyedVersion;
+	public float blastRadius;
+	public float explosionPower;
+	public LayerMask explosionLayer;
+
+	private Collider[] hitColliders;
+	void OnMouseDown(){
+		Instantiate(destroyedVersion, transform.position, transform.rotation);
+		Destroy(gameObject);
+		Explode(transform.position);
+	}
+
+	void Explode(Vector3 explosionCenter){
+		hitColliders = Physics.OverlapSphere(explosionCenter, blastRadius, explosionLayer);
+
+		foreach (Collider hitCol in hitColliders)
+		{
+			hitCol.GetComponent<Rigidbody>().isKinematic = false;
+			hitCol.GetComponent<Rigidbody>().AddExplosionForce(explosionPower,explosionCenter,blastRadius,1,ForceMode.Impulse);
+		}
+	}
+}
