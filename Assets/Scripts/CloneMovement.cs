62a8de4c93feed47867e63ea0f15c2b070b03f72
Dubito-mac <peng86david@gmail.com>
2019-09-16 15:31:59 +0800

    clone behavior -> more player-like

---
+++ Assets/Scripts/CloneMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneMovement : MonoBehaviour
+{
+    Rigidbody body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     void FixedUpdate()
+    {
+        body.MovePosition(body.position + transform.forward * 5f * Time.fixedDeltaTime);
+    }
+}
